5bfb243f5d4d2f4327dfb3b9601a6568db849162
colmike <michaelngigi76@gmail.com>
2020-08-06 09:55:22 +0300

    add src folder

--- src/main/java/com/michaelmbugua/expenseTrackerApi/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.michaelmbugua.expenseTrackerApi;
-
-public class Constants {
-
-    public static final String API_SECRET_KEY = "myVeryAwesomeSuperSecretAPIKey";
-    public static final long TOKEN_VALIDITY = 2 * 60 * 60 * 1000;
-
-}
